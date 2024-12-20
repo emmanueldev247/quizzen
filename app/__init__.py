7f85e10f5f5b2d7faf522b36ff2f11ba94f7ab25
emmanueldev247 <mailemmydee@gmail.com>
2024-12-20 20:02:59 +0000

    updated: template rendering

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
 
-app = Flask(__name__,
-             static_folder='app/static', 
+app = Flask(__name__, static_folder='static',
              static_url_path='/assets',
-             template_folder='app/templates'
+             template_folder='templates'
             )
 app.config['SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', secrets.token_urlsafe(32))
 
