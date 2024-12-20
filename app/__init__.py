f0b7956d1fb6b88f284c7ea9bb5d5f47947efb7a
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-20 21:16:04 +0100

    adjusted: static_url_path

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from flask_login import LoginManager
 from flask_session import Session
 
 app = Flask(__name__, static_folder='static',
-             static_url_path='/assets',
+             static_url_path='/quizzen/assets',
              template_folder='templates'
             )
 app.config['SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', secrets.token_urlsafe(32))
