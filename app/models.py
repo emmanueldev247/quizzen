684dc0b2e0adf793874bc1d254fca30e76e3e587
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-24 00:31:55 +0100

    increased: password_hash length to 255

--- app/models.py
+++ app/models.py
@@ -44,7 +44,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(100), nullable=False)
     last_name = db.Column(db.String(100), nullable=False)
     date_of_birth = db.Column(db.Date, nullable=False)
