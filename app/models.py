7404c2b80bab02d2997d6fb04af3385baad4617e
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-23 16:35:58 +0100

    fixed: error - type "role_types" does not exist

--- app/models.py
+++ app/models.py
@@ -16,7 +16,7 @@ Imports:
 from app.extensions import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from sqlalchemy.types import Enum
+from sqlalchemy.dialects.postgresql import ENUM as Enum
 
 
 class User(db.Model):
@@ -49,7 +49,7 @@ class User(db.Model):
     last_name = db.Column(db.String(100), nullable=False)
     date_of_birth = db.Column(db.Date, nullable=False)
     profile_picture = db.Column(db.String(255))
-    role = db.Column(Enum('user', 'admin', name='role_types'), default='user')
+    role = db.Column(Enum('user', 'admin', name='role_types', create_type=True), default='user')
     gender = db.Column(db.String(10), nullable=False)
     quiz_history = db.relationship('QuizHistory', backref='quiz',
                                    cascade='all, delete-orphan', lazy=True)
