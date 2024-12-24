6342229f88fb0b5d0df76beadd3bb843c8b76865
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-24 04:11:17 +0100

    fixed: bug in notification

--- app/static/js/login-reset-page.js
+++ app/static/js/login-reset-page.js
@@ -126,8 +126,8 @@ function showNotification(message, type) {
   const notification = document.getElementById("notification");
   notification.textContent = message;
   notification.className = `notification ${type}`;
-  notification.classList.add = "visible";
+  notification.classList.add("visible");
   setTimeout(() => {
-    notification.classList.remove = "visible";
+    notification.classList.remove("visible");
   }, 5000);
 }
