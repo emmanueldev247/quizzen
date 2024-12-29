79345e44a8d411fa1cee2500f06f8530090fb5ec
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-29 15:51:46 +0100

    added: ZoomIn

--- app/static/js/reset_password_error.js
+++ app/static/js/reset_password_error.js
@@ -1,14 +1,18 @@
-import { showElements } from "./utils.js";
-
 // Get the elements
 const errorContainer = document.getElementById("error-container");
 const loginButton = document.getElementById("back-to-login");
 
 // Initialize by showing the signup form
+
+function showErrElements(...elements) {
+  elements.forEach(element => {
+      element.classList.add("zoom-in");
+  });
+}
 document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    showElements(errorContainer);
-  }, 2000);
+    showErrElements(errorContainer);
+  }, 500);
 });
 
 loginButton.addEventListener("click", () => window.location.href = "/quizzen/login");
