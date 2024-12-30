6e8a923a5e47e6ee2ef62ec8d0903dc3764ce68a
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-30 19:08:41 +0100

    fixed: error

--- run.py
+++ run.py
@@ -8,6 +8,6 @@ app = create_app()
 if __name__ == "__main__":
     port = int(os.getenv('FLASK_RUN_PORT', 5000))
     host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
-    app.run(host=host, port=port)
-    print(app.url_map)
     logger.info("----- Quizzen app initialized -----")
+    #print(app.url_map)
+    app.run(host=host, port=port)
