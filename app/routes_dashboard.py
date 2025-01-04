3a5c7f8d879b2d6fdee24caf090fc83efddbca56
Emmanuel Ademola <mailemmydee@gmail.com>
2025-01-05 00:28:41 +0100

    logic updated

--- app/routes_dashboard.py
+++ app/routes_dashboard.py
@@ -1,6 +1,10 @@
 """
     Defined routes:
-      -
+      - '/dashboard' -> dashboard
+      - '/quiz/new' -> create_quiz
+      - '/quiz/<quiz_id>/edit' -> edit_quiz
+      - '/quiz/<quiz_id>/question/new' -> create_question
+      - '/quiz/<quiz_id>/question/<question_id>/edit' -> edit_question
 """
 from app.extensions import db, limiter
 from app.models import (
@@ -8,7 +12,7 @@ from app.models import (
     Question, Quiz, QuizHistory, User
 )
 from app.routes import (
-    auth_required, full_bp, logger, ratelimit_exceeded
+    full_bp, logger, ratelimit_exceeded
 )
 from flask import (
     current_app, flash, jsonify,
@@ -18,6 +22,56 @@ from flask import (
 from flask_limiter.errors import RateLimitExceeded
 
 
+
+def auth_required(f):
+    """Auth required function"""
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        """Auth required decorator"""
+        logger.info(f"Auth Attempt")
+        if 'user_id' not in session:
+            logger.error(f"Session token missing")
+            flash("log in", "error")
+            return redirect(url_for('full_bp.login'))
+
+        try:
+            user_id = session['user_id']
+            current_user = User.query.get(user_id)
+
+            if not current_user:
+                logger.error("Invalid Token")
+                flash("log in", "error")
+                return redirect(url_for('full_bp.login'))
+
+        except Exception as e:
+            logger.error(f"Invalid Token, Error: {str(e)}")
+            flash("log in", "error")
+            return redirect(url_for('full_bp.login'))
+
+        return f(current_user, *args, **kwargs)
+    return decorated
+
+def admin_check(f):
+    """Grants role privileges"""
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        """Auth required decorator"""
+        logger.info(f"Auth Attempt")
+        if 'user_role' not in session:
+            logger.error(f"Session token missing")
+            flash("log in", "error")
+            return redirect(url_for('full_bp.login'))
+        try:
+            if session['user_role'] != 'admin':
+                return redirect(url_for('full_bp.dashboard'))        
+        except Exception as e:
+            logger.error(f"Invalid Token, Error: {str(e)}")
+            flash("log in", "error")
+            return redirect(url_for('full_bp.login'))
+        return f(*args, **kwargs)
+    return decorated
+
+
 @full_bp.errorhandler(RateLimitExceeded)
 def handle_rate_limit_exceeded(e):
     return rate_limit_exceeded(e)
@@ -25,7 +79,18 @@ def handle_rate_limit_exceeded(e):
 
 @full_bp.route('/dashboard')
 @auth_required
-def dashboard(current_user):
+def user_dashboard(current_user):
+    if current_user.role == 'admin':
+        return redirect(url_for('full_bp.admin_dashboard'))
+    return render_template('user_dashboard.html', user=current_user)
+
+
+
+@full_bp.route('/admin/dashboard')
+@admin_check
+@auth_required
+def admin_dashboard(current_user):
+    """Admin-specific dashboard."""
     logger.debug(f"{request.method} - Dashboard")
     user = User.query.get(current_user.id)
     if not user:
@@ -40,65 +105,199 @@ def dashboard(current_user):
     ).order_by(
         Notification.date_sent.desc()
     ).all()
+    categories = Category.query.order_by(Category.name.asc()).all()
 
     return render_template(
         'dashboard.html',
         title='Dashboard',
         user=user,
         quizzes=quizzes,
+        categories=categories,
         leaderboard=leaderboard,
         notifications=notifications
     )
 
 
-@full_bp.route('/create_quiz')
+@full_bp.route('/quiz/new', methods=['POST'])
+@admin_check
 @auth_required
+@limiter.limit("5 per minute")
 def create_quiz(current_user):
     """Creates a new quiz"""
-    limiter.limit("5 per minute")(lambda: None)()
-    logger.debug(f"{request.method} - Create quiz attempt")
     try:
-        new_quiz = Quiz(
-            title="Untitled Quiz",
-            created_by=current_user.id,
-            duration=30
+        logger.debug(f"{request.method} - Create quiz attempt")
+
+        data = request.get_json() if request.is_json else request.form
+        title = data.get('title', '').strip()
+        description = data.get('description', '').strip()
+        category_id = data.get('category')
+        duration = data.get('duration', 0)
+        
+        if not title:
+            return jsonify({'success': False, 'message': 'Title is required.'}), 400
+        if not category_id:
+            return jsonify({'success': False, 'message': 'Category is required.'}), 400
+        if not duration or int(duration) <= 0:
+            return jsonify({'success': False, 'message': 'Invalid duration.'}), 400
+
+       
+        quiz = Quiz(
+            title=title,
+            description=description,
+            category_id=category_id,
+            duration=int(duration),
+            created_by=current_user.id
         )
 
-        db.session.add(new_quiz)
+        db.session.add(quiz)
         db.session.commit()
-
-        new_question = Question(
-            quiz_id=new_quiz.id,
-            question_text="",
-            question_type="multiple_choice",
-            points=1
-        )   
-
-        db.session.add(new_question)
-        db.session.commit()     
 
         return redirect(
             url_for(
-                'full_bp.edit_question',
-                quiz_id=new_quiz.id,
-                question_id=new_question.id
+                'full_bp.edit_quiz',
+                quiz_id=quiz.id
             )
         )
     except Exception as e:
         db.session.rollback()
         logger.error(f"Error during quiz creation: {str(e)}")
         flash("An error occurred while creating the quiz. Please try again.", "error")
+        return redirect(url_for('full_bp.admin_dashboard'))
+
+
+
+@full_bp.route('/quiz/<quiz_id>/edit', methods= ['GET', 'POST', 'PUT', 'DELETE'])
+@admin_check
+@auth_required
+def edit_quiz(current_user, quiz_id):
+    logger.info(f"Editting quiz attempt")
+    quiz = Quiz.query.get_or_404(quiz_id)
+
+    if quiz.created_by != current_user.id:
+        flash("You are not authorized to edit this quiz", "error")
         return redirect(url_for('full_bp.dashboard'))
 
-    # categories = Category.query.order_by(Category.name.asc()).all()
-    # return render_template('create_quiz.html', categories=categories)
+    logger.info(f"Editting quiz: {quiz.id}")
+    if request.method == 'POST': # Adding a question
+        try:
+            limiter.limit("5 per minute")(lambda: None)()
+            data = request.get_json() if request.is_json else request.form
+            
+            question_text = data.get('question', '').strip()
+            question_type = data.get('question_type', '').strip()
+            points = int(data.get('points', '0').strip())
+            answer_choices = data.getlist('answer_choices', '') 
+
+            if not all([question_text, question_type]):
+                return jsonify({"success": False, "message": "Invalid question details."}), 400
+            if question_type == 'multiple_choice' and len(answer_choices) < 2:
+                return jsonify({"success": False, "message": "Multiple choice questions need at least two options."}), 400
+
+            new_question = Question(
+                quiz_id = quiz_id,
+                question_text = question_text,
+                question_type = question_type,
+                points=points
+            )
+            db.session.add(new_question)
+            db.commit()
+            for option in answer_choices:
+                new_question.answer_choices.append(
+                    AnswerChoice(
+                        question_id=new_question.id,
+                        text=option["text"],
+                        is_correct=option['isCorrect']
+                    )
+                )
+
+            db.session.add(new_question)
+            quiz.calculate_max_score()
+            db.session.commit()
+            return jsonify({"success": True, "message": "Question added successfully."}), 201
+        except Exception as e:
+            db.session.rollback()
+            logger.error(f"Error during question addition: {str(e)}")
+            return jsonify({"success": False, "message": "Error adding question.", "error": str(e)}), 500
+
+    elif request.method == "PUT":
+        try:
+            limiter.limit("10 per minute")(lambda: None)()
+            data = request.get_json() if request.is_json else request.form
+            
+
+            quiz.title = data.get("title", quiz.title) 
+            quiz.description = data.get("description", quiz.description) 
+            quiz.category = data.get("category", quiz.category) 
+            quiz.duration = int(data.get("duration", quiz.duration)) 
+            quiz.public = data.get("title", quiz.public)
+    
+            quiz.calculate_max_score()
+            db.session.commit()
+
+            return jsonify({"success": True, "message": "Quiz updated successfully."}), 200
+        except Exception as e:
+            db.session.rollback()
+            logger.error(f"Error updating quiz: {str(e)}")
+        
+            return jsonify({
+                "success": False,
+                "message": "Error updating quiz",
+                "error": str(e)
+            }), 500
+
+    elif request.method == "DELETE":
+        try:
+            db.session.delete(quiz)
+            db.session.commit()
+            return jsonify({"success": True, "message": "Quiz deleted successfully."}), 200 
+        except Exception as e:
+            logger.error(f"Error deleting quiz: {str(e)}")
+            return jsonify({
+                "success": False,
+                "message": "Error deleting quiz",
+                "error": str(e)
+            }), 500
+
+
+    return render_template('edit_quiz.html', quiz=quiz, title=quiz.title)
+
+
+
+@full_bp.route('/quiz/<quiz_id>/question/new')
+@admin_check
+@auth_required
+def create_question(current_user, quiz_id):
+    """Create a new question and redirect to edit page."""
+    quiz = Quiz.query.get_or_404(quiz_id)
+
+    if quiz.created_by != current_user.id:
+        flash("Unauthorized access", "error")
+        return redirect(url_for('full_bp.dashboard'))
+
+    new_question = Question(
+        quiz_id=quiz_id,
+        question_text=""
+    )
+
+    db.session.add(new_question)
+    db.session.commit()
+
+    return redirect(
+        url_for(
+            'full_bp.edit_question', 
+            quiz_id=quiz_id,
+            question_id=new_question.id
+        )
+    )
 
 
 @full_bp.route('/quiz/<quiz_id>/question/<question_id>', methods=['GET', 'POST', 'DELETE'])
 @full_bp.route('/quiz/<quiz_id>/question/<question_id>/edit', methods=['GET', 'POST', 'DELETE'])
+@admin_check
 @auth_required
 def edit_question(current_user, quiz_id, question_id):
     """Edit a question"""
+    limiter.limit("5 per minute")(lambda: None)()
     quiz = Quiz.query.get_or_404(quiz_id)
     question = Question.query.get_or_404(question_id)
     question.answer_choices = AnswerChoice.query.filter_by(question_id=question.id).all()
@@ -108,28 +307,12 @@ def edit_question(current_user, quiz_id, question_id):
         flash("Unauthorized access.", "danger")
         logger.error("No owner")
         return redirect(url_for('full_bp.dashboard'))
-    
-    if request.method == 'DELETE':
-        try:
-            db.session.delete(question)
-            quiz.calculate_max_score()
-            db.session.commit()
-            return jsonify({"success": True, "message": "Question deleted successfully."}), 200 
-        except Exception as e:
-            logger.error(f"Error deleting question: {str(e)}")
-            return jsonify({
-                "success": False,
-                "message": "Error deleting question",
-                "error": str(e)
-            }), 500
 
-    if request.method == 'POST':
-        try:
-            if request.is_json:
-                data = request.get_json()
-            else:
-                data = request.form
 
+    if request.method == 'POST': # do PUT also PUT vs PATCH
+        try:
+            data = request.get_json() if request.is_json else request.form
+            
             question_text = data.get('question', '').strip()
             question_type = data.get('questionType', '').strip()
             is_multiple_response = data.get('multipleResponse', 'False')
@@ -171,79 +354,18 @@ def edit_question(current_user, quiz_id, question_id):
                 "error": str(e)
             })
 
-    return render_template('edit_question.html', quiz=quiz, question=question)
-
-
-@full_bp.route('/quiz/<quiz_id>/edit', methods= ['GET', 'POST'])
-@auth_required
-def edit_quiz(current_user, quiz_id):
-    logger.info(f"Editting quiz attempt")
-    quiz = Quiz.query.get_or_404(quiz_id)
-
-    if quiz.created_by != current_user.id:
-        flash("You are not authorized to edit this quiz", "error")
-        return redirect(url_for('full_bp.dashboard'))
-
-    logger.info(f"Editting quiz: {quiz.id}")
-    if request.method == 'POST':
+    elif request.method == 'DELETE':
         try:
-            if request.is_json:
-                data = request.get_json()
-            else:
-                data = request.form
-            
-            question_text = data.get('question', '').strip()
-            answer_choices = data.getlist('answer_choices', '') 
-            question_type = data.get('question_type', '').strip()
-            points = int(data.get('points', '0').strip())
-
-            if question_type == 'multiple_choice' and len(answer_choices) < 2:
-                flash('Multiple choice questions must have at least two options.', 
-                    'danger')
-                return redirect(url_for('full_bp.edit_quiz', quiz_id=quiz_id))
-
-            new_question = Question(
-                quiz_id = quiz_id,
-                question_text = question_text,
-                answer_choices = answer_choices,
-                question_type = question_type 
-            )
-
-            db.session.add(new_question)
+            db.session.delete(question)
             quiz.calculate_max_score()
             db.session.commit()
-            flash('Question added successfully', 'success')
+            return jsonify({"success": True, "message": "Question deleted successfully."}), 200 
         except Exception as e:
-            db.session.rollback()
-            flash('Error adding question. Please try again.', 'danger')
-    return render_template('edit_quiz.html', quiz=quiz, title=quiz.title)
+            logger.error(f"Error deleting question: {str(e)}")
+            return jsonify({
+                "success": False,
+                "message": "Error deleting question",
+                "error": str(e)
+            }), 500
 
-    
-@full_bp.route('/quiz/<quiz_id>/question/new')
-@auth_required
-def create_question(current_user, quiz_id):
-    """Create a new question and redirect to edit page."""
-    quiz = Quiz.query.get_or_404(quiz_id)
-
-    if quiz.created_by != current_user.id:
-        flash("Unauthorized access", "error")
-        return redirect(url_for('full_bp.dashboard'))
-
-    new_question = Question(
-        quiz_id=quiz_id,
-        question_text="",
-        answer_choices=[],
-        question_type="multiple_choice"
-    )
-
-    db.session.add(new_question)
-    db.session.commit()
-
-    return redirect(
-        url_for(
-            'full_bp.edit_question', 
-            quiz_id=quiz_id,
-            question_id=new_question.id
-        )
-    )
-
+    return render_template('edit_question.html', quiz=quiz, question=question)
