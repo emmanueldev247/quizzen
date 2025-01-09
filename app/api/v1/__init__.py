4933e5f2382ebc2bb3ea7794c98422c07fb4756e
Emmanuel Ademola <mailemmydee@gmail.com>
2025-01-10 00:17:13 +0100

    api started

---
+++ app/api/v1/__init__.py
@@ -0,0 +1,5 @@
+from flask import blueprint
+
+api_v1 = Blueprint('api_v1', __name__, url_prefix='/quizzen/api/v1')
+
+from . import auth, quizzes, questions
---
+++ app/api/v1/auth.py
@@ -0,0 +1,26 @@
+from flask import request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt
+from app.models import User
+from app.extensions import db, blacklist_redis
+
+@api_v1.route('/connect', methods=['POST'])
+def login():
+    data = request.json
+
+    email = unicodedata.normalize(
+                'NFKC', data.get('email', '').strip().lower()
+            )
+    password = data.get('password', '')
+    user = User.query.filter_by(email=email).first()
+    if user and user.check_password(password):
+        access_token = create_access_token(identity=user.id)
+        return jsonify({"success": True, access_token:access_token}), 200
+    return jsonify({"success": False, "error": "Invalid credentials"}), 401
+
+
+@api_v1.route('/disconnect', methods=['POST'])
+@jwt_required
+def logout():
+    jti = get_jwt()["jti"]
+    blacklist_redis.set(jti, "blacklisted", ex=60*60)
+    return jsonify({"success": True, "msg": "Token successfully disconnectd"})
