9b8c2896b63f57a073745d7ac054e6f2ff5282a7
Emmanuel Ademola <mailemmydee@gmail.com>
2025-01-11 16:53:01 +0100

    added: limits

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,11 +1,12 @@
+import unicodedata
 from flask import request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt
 from app.models import User
-from app.extensions import db, blacklist_redis
+from app.extensions import blacklist_redis, limiter
 from . import api_v1
-import unicodedata
 
 @api_v1.route('/connect', methods=['POST'])
+@limiter.limit("5 per minute")
 def login():
     data = request.json
 
@@ -16,13 +17,14 @@ def login():
     user = User.query.filter_by(email=email).first()
     if user and user.check_password(password):
         access_token = create_access_token(identity=user.id)
-        return jsonify({"success": True, access_token:access_token}), 200
+        return jsonify({"success": True, "access_token":access_token}), 200
     return jsonify({"success": False, "error": "Invalid credentials"}), 401
 
 
 @api_v1.route('/disconnect', methods=['POST'])
-@jwt_required
+@jwt_required()
+@limiter.limit("5 per minute")
 def logout():
     jti = get_jwt()["jti"]
     blacklist_redis.set(jti, "blacklisted", ex=60*60)
-    return jsonify({"success": True, "msg": "Token successfully disconnectd"})
+    return jsonify({"success": True, "msg": "Token successfully disconnected"})
