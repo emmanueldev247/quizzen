346694a499ed2e586395e5ace68fad086fbcc25a
Emmanuel Ademola <mailemmydee@gmail.com>
2025-01-17 17:54:06 +0100

    put all in DOMContentLoaded

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -6,177 +6,186 @@ import {
   togglePasswordVisibility,
 } from "./utils.js";
 
-togglePasswordVisibility("#password");
-
-// Get the elements
-const loginForm = document.getElementById("login-form");
-const resetPasswordForm = document.getElementById("reset-password-form");
-const socialLogin = document.getElementById("social-login");
-const signupLink = document.getElementById("signup-link");
-const forgotPasswordLink = document.getElementById("forgot-password");
-const backToLoginLink = document.getElementById("back-to-login");
-
-const resetPasswordElements = Array.from(
-  resetPasswordForm.querySelectorAll("h2, p, input, button, a")
-);
-const loginFormElements = Array.from(
-  loginForm.querySelectorAll("h2, input, button, a")
-);
-
-// Function to show the login form
-function showLoginForm() {
-  hideElements(resetPasswordForm);
-  showElements(loginForm, socialLogin, signupLink);
-}
-
-// Function to show the reset password form
-function showResetPasswordForm() {
-  hideElements(loginForm, socialLogin, signupLink);
-  showElements(resetPasswordForm);
-}
-
-// Event listeners for toggling views
-forgotPasswordLink.addEventListener("click", function () {
-  loginForm.style.display = socialLogin.style.display = "none";
-  resetPasswordForm.style.display = "block";
-  animateElements(resetPasswordElements);
-  showResetPasswordForm();
-});
-
-backToLoginLink.addEventListener("click", () => {
-  resetPasswordForm.style.display = "none";
-  loginForm.style.display = "flex";
-  socialLogin.style.display = "block";
-  animateElements(loginFormElements);
-  showLoginForm();
-});
-
-// Initialize by showing the login form
 document.addEventListener("DOMContentLoaded", () => {
+  // Get the elements
+  const loginForm = document.getElementById("login-form");
+  const resetPasswordForm = document.getElementById("reset-password-form");
+  const socialLogin = document.getElementById("social-login");
+  const signupLink = document.getElementById("signup-link");
+  const forgotPasswordLink = document.getElementById("forgot-password");
+  const backToLoginLink = document.getElementById("back-to-login");
+  
+  const resetPasswordElements = Array.from(
+    resetPasswordForm.querySelectorAll("h2, p, input, button, a")
+  );
+  const loginFormElements = Array.from(
+    loginForm.querySelectorAll("h2, input, button, a")
+  );
+  
+  // Function to show the login form
+  function showLoginForm() {
+    hideElements(resetPasswordForm);
+    showElements(loginForm, socialLogin, signupLink);
+  }
+  
+  // Function to show the reset password form
+  function showResetPasswordForm() {
+    hideElements(loginForm, socialLogin, signupLink);
+    showElements(resetPasswordForm);
+  }
+  
+  // Initialize by showing the login form
   setTimeout(() => {
     showLoginForm();
   }, 200);
-});
 
-loginForm.addEventListener("submit", function (event) {
-  event.preventDefault();
+  togglePasswordVisibility("#password");
 
-  const formData = new FormData(this);
+  document
+    .querySelector(".signup-button")
+    .addEventListener(
+      "click",
+      () => (window.location.href = "/quizzen/signup")
+    );
 
-  const loginButton = document.getElementById("login-button");
-  const warningCard = document.getElementById("login-warning");
-  const loginWarningSpan = document.querySelector("#login-warning span");
+  // Event listeners for toggling views
+  forgotPasswordLink.addEventListener("click", function () {
+    loginForm.style.display = socialLogin.style.display = "none";
+    resetPasswordForm.style.display = "block";
+    animateElements(resetPasswordElements);
+    showResetPasswordForm();
+  });
 
-  loginButton.textContent = "Logging in...";
-  loginButton.disabled = true;
+  backToLoginLink.addEventListener("click", () => {
+    resetPasswordForm.style.display = "none";
+    loginForm.style.display = "flex";
+    socialLogin.style.display = "block";
+    animateElements(loginFormElements);
+    showLoginForm();
+  });
 
-  warningCard.style.display = "none";
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-  fetch("/quizzen/login", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        if (response.status === 401) {
-          loginWarningSpan.innerHTML = `Invalid username or password`;
-          warningCard.style.display = "flex";
-        } else if (response.status === 429) {
+    const formData = new FormData(this);
+
+    const loginButton = document.getElementById("login-button");
+    const warningCard = document.getElementById("login-warning");
+    const loginWarningSpan = document.querySelector("#login-warning span");
+
+    loginButton.textContent = "Logging in...";
+    loginButton.disabled = true;
+
+    warningCard.style.display = "none";
+
+    fetch("/quizzen/login", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 401) {
+            loginWarningSpan.innerHTML = `Invalid username or password`;
+            warningCard.style.display = "flex";
+          } else if (response.status === 429) {
+            showNotification(
+              "You have made too many requests in a short period. Please try again later",
+              "error"
+            );
+          } else
+            showNotification(
+              "Something went wrong. Please try again later",
+              "error"
+            );
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          showNotification("Login successful", "success");
+          window.location.href = "/quizzen/dashboard";
+        }
+      })
+      .catch((error) => {
+        if (error.message === "Failed to fetch")
           showNotification(
-            "You have made too many requests in a short period. Please try again later",
+            "Network error. Please check your connection",
             "error"
           );
-        } else
+      })
+      .finally(() => {
+        loginButton.disabled = false;
+        loginButton.textContent = "Log in";
+      });
+  });
+
+  resetPasswordForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    const resetPasswordButton = document.getElementById(
+      "reset-password-button"
+    );
+    const warningCard = document.getElementById("reset-password-warning");
+    const successCard = document.getElementById("reset-password-success");
+    const resetWarningSpan = document.querySelector(
+      "#reset-password-warning span"
+    );
+    const resetSuccessSpan = document.querySelector(
+      "#reset-password-success span"
+    );
+
+    resetPasswordButton.textContent = "Resetting password...";
+    resetPasswordButton.disabled = true;
+
+    warningCard.style.display = successCard.style.display = "none";
+
+    fetch("/quizzen/reset_password", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 404) {
+            resetWarningSpan.innerHTML = `No account associated with 
+            <span style="color: #d9534f;">${formData.get("email")}</span> .
+            <br>Please register to create an account.`;
+            warningCard.style.display = "flex";
+          } else if (response.status === 429) {
+            showNotification(
+              "You have made too many requests in a short period. Please try again later",
+              "error"
+            );
+          } else
+            showNotification(
+              "Something went wrong. Please try again later",
+              "error"
+            );
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          resetSuccessSpan.innerHTML = `A link has been sent to 
+          <span style="color: #009724;">${formData.get("email")}</span> .
+          <br>For your safety, this link expires in 30 minutes and can only be used once.`;
+          successCard.style.display = "flex";
+          showNotification("Please check your inbox or spam folder", "success");
+        }
+      })
+      .catch((error) => {
+        if (error.message === "Failed to fetch")
           showNotification(
-            "Something went wrong. Please try again later",
+            "Network error. Please check your connection",
             "error"
           );
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.success) {
-        showNotification("Login successful", "success");
-        window.location.href = "/quizzen/dashboard";
-      }
-    })
-    .catch((error) => {
-      if (error.message === "Failed to fetch")
-        showNotification(
-          "Network error. Please check your connection",
-          "error"
-        );
-    })
-    .finally(() => {
-      loginButton.disabled = false;
-      loginButton.textContent = "Log in";
-    });
-});
-
-resetPasswordForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const formData = new FormData(this);
-
-  const resetPasswordButton = document.getElementById("reset-password-button");
-  const warningCard = document.getElementById("reset-password-warning");
-  const successCard = document.getElementById("reset-password-success");
-  const resetWarningSpan = document.querySelector(
-    "#reset-password-warning span"
-  );
-  const resetSuccessSpan = document.querySelector(
-    "#reset-password-success span"
-  );
-
-  resetPasswordButton.textContent = "Resetting password...";
-  resetPasswordButton.disabled = true;
-
-  warningCard.style.display = successCard.style.display = "none";
-
-  fetch("/quizzen/reset_password", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          resetWarningSpan.innerHTML = `No account associated with 
-          <span style="color: #d9534f;">${formData.get("email")}</span> .
-          <br>Please register to create an account.`;
-          warningCard.style.display = "flex";
-        } else if (response.status === 429) {
-          showNotification(
-            "You have made too many requests in a short period. Please try again later",
-            "error"
-          );
-        } else
-          showNotification(
-            "Something went wrong. Please try again later",
-            "error"
-          );
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.success) {
-        resetSuccessSpan.innerHTML = `A link has been sent to 
-        <span style="color: #009724;">${formData.get("email")}</span> .
-        <br>For your safety, this link expires in 30 minutes and can only be used once.`;
-        successCard.style.display = "flex";
-        showNotification("Please check your inbox or spam folder", "success");
-      }
-    })
-    .catch((error) => {
-      if (error.message === "Failed to fetch")
-        showNotification(
-          "Network error. Please check your connection",
-          "error"
-        );
-    })
-    .finally(() => {
-      resetPasswordButton.disabled = false;
-      resetPasswordButton.textContent = "Reset password";
-    });
+      })
+      .finally(() => {
+        resetPasswordButton.disabled = false;
+        resetPasswordButton.textContent = "Reset password";
+      });
+  });
 });
--- app/static/js/signup.js
+++ app/static/js/signup.js
@@ -4,209 +4,213 @@ import {
   togglePasswordVisibility,
 } from "./utils.js";
 
-// Get the elements
-const signupForm = document.getElementById("signup-form");
-const socialSignup = document.getElementById("social-signup");
-const loginLink = document.getElementById("login-link");
-
-// Initialize by showing the signup form
 document.addEventListener("DOMContentLoaded", () => {
+  // Get the elements
+  const signupForm = document.getElementById("signup-form");
+  const socialSignup = document.getElementById("social-signup");
+  const loginLink = document.getElementById("login-link");
+
+  function scrollToBottom() {
+    signupForm.scrollTop = signupForm.scrollHeight;
+  }
+
+  // Initialize by showing the signup form
   setTimeout(() => {
     showElements(signupForm, socialSignup, loginLink);
   }, 200);
-});
 
-togglePasswordVisibility("#password", "#c_password");
+  togglePasswordVisibility("#password", "#c_password");
 
-// Role icon select
-document.querySelectorAll(".role-option").forEach((option) => {
-  option.addEventListener("click", function () {
-    document.getElementById("role").value = this.dataset.role;
-    document
-      .querySelectorAll(".role-option")
-      .forEach((opt) => opt.classList.remove("selected"));
-    this.classList.add("selected");
-  });
-});
+  document
+    .querySelector(".login-button")
+    .addEventListener("click", () => (window.location.href = "/quizzen/login"));
 
-function scrollToBottom() {
-  signupForm.scrollTop = signupForm.scrollHeight;
-}
-
-// paginated registration page buttons
-const contBtnEmail = document.getElementById("continue-button-email");
-const contBtnNames = document.getElementById("continue-button-names");
-const contBtnRole = document.getElementById("continue-button-role");
-const submitBtn = document.getElementById("submit-button");
-
-const firstNameDiv = document.getElementById("first-name-container");
-const roleDiv = document.getElementById("role-container");
-const passwordDiv = document.getElementById("password-container");
-
-contBtnEmail.addEventListener("click", () => {
-  const email = document.getElementById("email").value;
-  const emailError = document.getElementById("email-error");
-
-  if (!email) {
-    emailError.textContent = "Please enter your email address";
-    emailError.style.display = "block";
-    return;
-  }
-  emailError.style.display =
-    contBtnEmail.style.display =
-    socialSignup.style.display =
-      "none";
-  firstNameDiv.style.display = "block";
-  scrollToBottom();
-});
-
-contBtnNames.addEventListener("click", () => {
-  const firstName = document.getElementById("first_name").value;
-  const lastName = document.getElementById("last_name").value;
-  const firstNameError = document.getElementById("first-name-error");
-  const lastNameError = document.getElementById("last-name-error");
-  firstNameError.style.display = lastNameError.style.display = "none";
-
-  if (!firstName) {
-    firstNameError.textContent = "Please enter your first name";
-    firstNameError.style.display = "block";
-    return;
-  }
-
-  if (!lastName) {
-    lastNameError.textContent = "Please enter your last name";
-    lastNameError.style.display = "block";
-    return;
-  }
-
-  if (firstName && lastName) {
-    contBtnNames.style.display = "none";
-    roleDiv.style.display = "block";
-    scrollToBottom();
-  }
-});
-
-contBtnRole.addEventListener("click", () => {
-  const role = document.getElementById("role").value;
-  const roleError = document.getElementById("role-error");
-  roleError.style.display = "none";
-
-  if (!role) {
-    roleError.textContent = "Please select what best describes you";
-    roleError.style.display = "block";
-    return;
-  }
-  contBtnRole.style.display = "none";
-  // roleDiv.style.display = "none";
-  passwordDiv.style.display = "block";
-  scrollToBottom();
-});
-
-submitBtn.addEventListener("click", (event) => {
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("c_password").value;
-  const passwordError = document.getElementById("password-error");
-  const cPasswordError = document.getElementById("c_password-error");
-  const cPasswordError2 = document.getElementById("c_password-error2");
-  const passwordStrengthError = document.getElementById(
-    "password-strength-error"
-  );
-
-  const passwordStrengthRegex =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|:;"'<>,.?/~`\\])[A-Za-z\d !@#$%^&*()_\-+={}[\]|:;"'<>,.?/~`\\]{8,}$/;
-
-  // Reset error displays
-  passwordError.style.display = passwordStrengthError.style.display = "none";
-  cPasswordError2.style.display = cPasswordError.style.display = "none";
-
-  if (!password) {
-    passwordError.textContent = "Password is required";
-    passwordError.style.display = "block";
-    event.preventDefault();
-    return;
-  }
-
-  if (!passwordStrengthRegex.test(password)) {
-    passwordStrengthError.textContent =
-      "Password must be at least 8 characters long and include letters, numbers, and a special character";
-    passwordStrengthError.style.display = "block";
-    event.preventDefault();
-    return;
-  }
-
-  if (!confirmPassword) {
-    cPasswordError.textContent = "Please confirm your password";
-    cPasswordError.style.display = "block";
-    event.preventDefault();
-    return;
-  }
-
-  if (password !== confirmPassword) {
-    cPasswordError2.textContent = "Passwords do not match!";
-    cPasswordError2.style.display = "block";
-    event.preventDefault();
-    return;
-  }
-
-  passwordError.style.display = passwordStrengthError.style.display = "none";
-  cPasswordError2.style.display = cPasswordError.style.display = "none";
-});
-
-signupForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  submitBtn.textContent = "Registering...";
-  submitBtn.disabled = true;
-
-  const formData = new FormData(this);
-
-  // Send POST request to backend
-  fetch("/quizzen/signup", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        if (response.status === 400) {
-          showNotification(
-            "An account already exists for this email. Please use the button above to log in or register another email",
-            "error"
-          );
-        } else if (response.status === 429) {
-          showNotification(
-            "You have made too many requests in a short period. Please try again later",
-            "error"
-          );
-        } else {
-          showNotification(
-            "Something went wrong. Please try again later",
-            "error"
-          );
-        }
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.success) {
-        showNotification(
-          "Registration successful! You can now log in",
-          "success"
-        );
-        setTimeout(() => {
-          window.location.href = "/quizzen/login";
-        }, 3000);
-      }
-    })
-    .catch((error) => {
-      if (error.message === "Failed to fetch")
-        showNotification(
-          "Network error. Please check your connection",
-          "error"
-        );
-    })
-    .finally(() => {
-      submitBtn.textContent = "Register";
-      submitBtn.disabled = false;
+  // Role icon select
+  document.querySelectorAll(".role-option").forEach((option) => {
+    option.addEventListener("click", function () {
+      document.getElementById("role").value = this.dataset.role;
+      document
+        .querySelectorAll(".role-option")
+        .forEach((opt) => opt.classList.remove("selected"));
+      this.classList.add("selected");
     });
+  });
+
+  // paginated registration page buttons
+  const contBtnEmail = document.getElementById("continue-button-email");
+  const contBtnNames = document.getElementById("continue-button-names");
+  const contBtnRole = document.getElementById("continue-button-role");
+  const submitBtn = document.getElementById("submit-button");
+
+  const firstNameDiv = document.getElementById("first-name-container");
+  const roleDiv = document.getElementById("role-container");
+  const passwordDiv = document.getElementById("password-container");
+
+  contBtnEmail.addEventListener("click", () => {
+    const email = document.getElementById("email").value;
+    const emailError = document.getElementById("email-error");
+
+    if (!email) {
+      emailError.textContent = "Please enter your email address";
+      emailError.style.display = "block";
+      return;
+    }
+    emailError.style.display =
+      contBtnEmail.style.display =
+      socialSignup.style.display =
+        "none";
+    firstNameDiv.style.display = "block";
+    scrollToBottom();
+  });
+
+  contBtnNames.addEventListener("click", () => {
+    const firstName = document.getElementById("first_name").value;
+    const lastName = document.getElementById("last_name").value;
+    const firstNameError = document.getElementById("first-name-error");
+    const lastNameError = document.getElementById("last-name-error");
+    firstNameError.style.display = lastNameError.style.display = "none";
+
+    if (!firstName) {
+      firstNameError.textContent = "Please enter your first name";
+      firstNameError.style.display = "block";
+      return;
+    }
+
+    if (!lastName) {
+      lastNameError.textContent = "Please enter your last name";
+      lastNameError.style.display = "block";
+      return;
+    }
+
+    if (firstName && lastName) {
+      contBtnNames.style.display = "none";
+      roleDiv.style.display = "block";
+      scrollToBottom();
+    }
+  });
+
+  contBtnRole.addEventListener("click", () => {
+    const role = document.getElementById("role").value;
+    const roleError = document.getElementById("role-error");
+    roleError.style.display = "none";
+
+    if (!role) {
+      roleError.textContent = "Please select what best describes you";
+      roleError.style.display = "block";
+      return;
+    }
+    contBtnRole.style.display = "none";
+    // roleDiv.style.display = "none";
+    passwordDiv.style.display = "block";
+    scrollToBottom();
+  });
+
+  submitBtn.addEventListener("click", (event) => {
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("c_password").value;
+    const passwordError = document.getElementById("password-error");
+    const cPasswordError = document.getElementById("c_password-error");
+    const cPasswordError2 = document.getElementById("c_password-error2");
+    const passwordStrengthError = document.getElementById(
+      "password-strength-error"
+    );
+
+    const passwordStrengthRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()_\-+={}[\]|:;"'<>,.?/~`\\])[A-Za-z\d !@#$%^&*()_\-+={}[\]|:;"'<>,.?/~`\\]{8,}$/;
+
+    // Reset error displays
+    passwordError.style.display = passwordStrengthError.style.display = "none";
+    cPasswordError2.style.display = cPasswordError.style.display = "none";
+
+    if (!password) {
+      passwordError.textContent = "Password is required";
+      passwordError.style.display = "block";
+      event.preventDefault();
+      return;
+    }
+
+    if (!passwordStrengthRegex.test(password)) {
+      passwordStrengthError.textContent =
+        "Password must be at least 8 characters long and include letters, numbers, and a special character";
+      passwordStrengthError.style.display = "block";
+      event.preventDefault();
+      return;
+    }
+
+    if (!confirmPassword) {
+      cPasswordError.textContent = "Please confirm your password";
+      cPasswordError.style.display = "block";
+      event.preventDefault();
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      cPasswordError2.textContent = "Passwords do not match!";
+      cPasswordError2.style.display = "block";
+      event.preventDefault();
+      return;
+    }
+
+    passwordError.style.display = passwordStrengthError.style.display = "none";
+    cPasswordError2.style.display = cPasswordError.style.display = "none";
+  });
+
+  signupForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    submitBtn.textContent = "Registering...";
+    submitBtn.disabled = true;
+
+    const formData = new FormData(this);
+
+    // Send POST request to backend
+    fetch("/quizzen/signup", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          if (response.status === 400) {
+            showNotification(
+              "An account already exists for this email. Please use the button above to log in or register another email",
+              "error"
+            );
+          } else if (response.status === 429) {
+            showNotification(
+              "You have made too many requests in a short period. Please try again later",
+              "error"
+            );
+          } else {
+            showNotification(
+              "Something went wrong. Please try again later",
+              "error"
+            );
+          }
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          showNotification(
+            "Registration successful! You can now log in",
+            "success"
+          );
+          setTimeout(() => {
+            window.location.href = "/quizzen/login";
+          }, 3000);
+        }
+      })
+      .catch((error) => {
+        if (error.message === "Failed to fetch")
+          showNotification(
+            "Network error. Please check your connection",
+            "error"
+          );
+      })
+      .finally(() => {
+        submitBtn.textContent = "Register";
+        submitBtn.disabled = false;
+      });
+  });
 });
