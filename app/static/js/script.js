70d969c674b67175cc1c5cb81d66aa7325faf078
Emmanuel Ademola <mailemmydee@gmail.com>
2025-01-23 17:01:53 +0100

    updated

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,22 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   const backToTopBtn = document.getElementById("backToTop");
 
-  backToTopBtn.addEventListener("click", () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
+  if (backToTopBtn) {
+    backToTopBtn.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     });
-  });
 
-  window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
-      backToTopBtn.classList.add("show");
-    } else {
-      backToTopBtn.classList.remove("show");
-    }
-  });
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 300) {
+        backToTopBtn.classList.add("show");
+      } else {
+        backToTopBtn.classList.remove("show");
+      }
+    });
+  }
 
-  document.querySelectorAll(".quiz-card").forEach((card) => {
+  document.querySelectorAll(".index-quiz-card").forEach((card) => {
     card.addEventListener("click", () => {
       window.location.href = "/quizzen/signup";
     });
