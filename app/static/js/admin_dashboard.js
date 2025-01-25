6b5346b991a2252d0ee2c1caa0166e00eaa31331
Emmanuel Ademola <mailemmydee@gmail.com>
2025-01-25 16:00:02 +0100

    testing

--- app/static/js/admin_dashboard.js
+++ app/static/js/admin_dashboard.js
@@ -2,4 +2,22 @@ import { setActive } from "./utils.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   setActive(".nav-item:nth-child(2)", ".bottom-nav-item:nth-child(1)");
+
+  const takeQuizButtons = document.querySelectorAll(".take-btn");
+
+  takeQuizButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      button.disabled = true;
+      const loader = button.querySelector(".loader");
+      loader.style.display = "inline-block";
+      setTimeout(() => {
+        button.disabled = false;
+        loader.style.display = "none";
+      }, 5000);
+      const quizCard = this.closest(".quiz-card");
+      const quizId = quizCard.getAttribute("data-quiz-id");
+
+      // window.location.href = `/quizzen/take/quiz/${quizId}`;
+    });
+  });
 });
